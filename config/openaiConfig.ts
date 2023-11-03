400f5c411828bb72812f254b302e92b3113bd0dd
Kirill <kirillthepuzzles@gmail.com>
2023-11-03 23:32:50 +0400

    set gpt-4 model by default

--- config/openaiConfig.ts
+++ config/openaiConfig.ts
@@ -11,7 +11,7 @@ const configuration = new Configuration({
 export const openai = new OpenAIApi(configuration)
 
 const defaultConfig: Omit<CreateChatCompletionRequest, 'messages'> = {
-  model: 'gpt-3.5-turbo-0301',
+  model: 'gpt-4',
   temperature: 0.6,
   max_tokens: 700,
   top_p: 1,
